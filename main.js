1f1177ca0554acd0bd14840e843ce58dc984f14c
adjohn36 <132848782+adjohn36@users.noreply.github.com>
2024-07-07 03:07:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,15 @@
-const SHA256 = require("crypto-js/sha256");
-
-class Block {
-    constructor(index, timestamp, data, previousHash = ""){
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-    }
-
-    calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
-    }
-}
-
-class Blockchain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
-    }
-
-    createGenesisBlock() {
-        return new Block(0, "1/1/2024", "Genesis block", "0");
-        
-    }
-
-    getLatestBlock(){
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
-    }
-
-    isChainValid(){
-        for(let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-        return true;
-    }
-}
+const {Blockchain, Transaction} = require("./blockchain");
 
 let adamCoin = new Blockchain();
-adamCoin.addBlock(new Block(1, "7/4/2024", { amount: 4 }));
-adamCoin.addBlock(new Block(2, "7/5/2024", { amount: 10 }));
+adamCoin.addTransaction(new Transaction("address1", "address2", 100));
+adamCoin.addTransaction(new Transaction("address2", "address1", 50));
 
-/*console.log("Is blockchain valid? " + adamCoin.isChainValid());
+console.log("\n Starting the miner...");
+adamCoin.minePendingTransactions("xaviers-address");
 
-adamCoin.chain[1].data = { amount: 100 };
-adamCoin.chain[1].hash = adamCoin.chain[1].calculateHash();
+console.log("\n Balance of xavier is", adamCoin.getBalanceOfAddress("xaviers-address"));
 
-console.log("Is blockchain valid? " + adamCoin.isChainValid());
-*/
-console.log(JSON.stringify(adamCoin, null, 4));
+console.log("\n Starting the miner again...");
+adamCoin.minePendingTransactions("xaviers-address");
+
+console.log("\n Balance of xavier is", adamCoin.getBalanceOfAddress("xaviers-address"));
